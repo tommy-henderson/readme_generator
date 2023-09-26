07bfac9c28edf085a73afd794728ee2f38b61304
Tommy Henderson <tommyhenderson@Tommys-MBP.ht.home>
2023-09-25 21:19:53 -0500

    added question bank for inquire

---
+++ Develop/index.js
@@ -0,0 +1,59 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs')
+
+// TODO: Create an array of questions for user input
+const questions = () => {
+    return inquirer
+        .prompt([
+{
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?'
+},
+{
+    type: 'input',
+    name: 'description',
+    message: 'describe your project?'
+},
+{
+    type: 'input',
+    name: 'installation',
+    message: 'what are the steps to install your project?'
+},
+
+{
+    type: 'input',
+    name: 'usage',
+    message: 'how do you use your project?'
+},
+{
+    type: 'input',
+    name: 'license',
+    message: 'what license did you use for this project?'
+},
+{
+    type: 'input',
+    name: 'contributors',
+    message: 'Who contributed to this project?'
+},
+{
+    type: 'input',
+    name: 'tests',
+    message: 'how do you test this project?'
+},
+        ])
+}
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) { }
+
+// TODO: Create a function to initialize app
+function init() { }
+
+// Function call to initialize app
+init();
+
+// test application
